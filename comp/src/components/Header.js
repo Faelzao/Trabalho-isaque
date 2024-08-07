34cddbfafee9ce0c1a1b6ef025acf4a115b6a677
Faelzao <rrafa8982@gmail.com>
2024-08-07 17:51:44 +0000

    save 01

---
+++ comp/src/components/Header.js
@@ -0,0 +1,9 @@
+export default function Header() {
+    return (
+        <div>
+            <h1>
+                Rafael é muito tonto! Componente é aqui!!!!
+            </h1>
+        </div>
+    )
+}
